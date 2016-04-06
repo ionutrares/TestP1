4683dc8fe3616a20bf178d05f2f37b13ac272611
IonutRares <ionutrares_bucur@yahoo.com>
2016-04-06 22:31:45 +0300

    06.04.2016 Example for

--- src/ExampleFor.java
+++ src/ExampleFor.java
@@ -5,8 +5,8 @@ public class ExampleFor {
     public static void main(String[] args){
             // i++ i = i + 1
         // for (initial value; condition; increment/decrement)
-        for(int i = 0; i <=10; i++){
-            System.out.printf("i= " + i);
+        for(int i = 10; i > 0; i--){
+            System.out.println("i= " + i);
         }
     }
 }
