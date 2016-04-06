ef6bad30ce11d04f01087e0c61c96c5b1d153508
IonutRares <ionutrares_bucur@yahoo.com>
2016-04-06 23:06:39 +0300

    06.04.2016 Example If While Do

---
+++ src/ExampleFor.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IonutRares on 06.04.2016.
+ */
+public class ExampleFor {
+
+    public static void main(String[] args) {
+
+        // for(initial value; condition; increment/decrement
+            for(int i = 0; i <= 10; i++){
+                System.out.println("i = " + i);
+            }
+    }
+}
---
+++ src/MyFirstClass.java
@@ -0,0 +1,20 @@
+/**
+ * Created by IonutRares on 06.04.2016.
+ */
+public class MyFirstClass {
+
+    /*
+     * This is my first
+     * Java program
+     * I am new to java
+     */
+    public static void main(String[] args){
+        // My First Statemen
+        System.out.println("Hello World!");
+
+        int x = 10;
+        int y = 20;
+        int result = x + y;
+        System.out.println("result = " + result);
+    }
+}
