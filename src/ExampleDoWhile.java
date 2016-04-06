a3d18610f64a5b06c078036a63fa374adae84ac7
IonutRares <ionutrares_bucur@yahoo.com>
2016-04-06 22:52:30 +0300

    06.04.2016 Example for

---
+++ src/ExampleDoWhile.java
@@ -0,0 +1,15 @@
+/**
+ * Created by IonutRares on 06.04.2016.
+ */
+public class ExampleDoWhile {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        do {
+            System.out.println("x = " + x);
+            x--;
+        }while(x > 0);
+    }
+}
