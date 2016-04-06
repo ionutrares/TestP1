456d877281c2c9d176febb548fd1df7d40e52417
IonutRares <ionutrares_bucur@yahoo.com>
2016-04-06 22:06:20 +0300

    06.04.2016 work in progress

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // My first statemens
+        System.out.println("Hello World");
+
+        int x = 10;
+        int y = 3;
+        int result = x % y;
+        System.out.println("result = " + result);
     }
 }
