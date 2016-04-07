f501e9b579b0bcb5d8936667caba4406f6db7926
IonutRares <ionutrares_bucur@yahoo.com>
2016-04-07 23:15:40 +0300

    07.04.2016

---
+++ src/ExampleSwitchCase.java
@@ -0,0 +1,30 @@
+/**
+ * Created by IonutRares on 07.04.2016.
+ */
+public class ExampleSwitchCase {
+
+    public static void maine(String[] args){
+
+        int j = 5;
+
+        switch(j){
+
+            case 0:
+                System.out.println("Value is 0");
+                break;
+            case 1:
+                System.out.println("Values is 1");
+                break;
+            case 2:
+                System.out.println("Values is 2");
+                break;
+            case 3:
+                System.out.println("Values is 3");
+                break;
+            default:
+                System.out.println("No Value");
+                break;
+
+        }
+    }
+}
